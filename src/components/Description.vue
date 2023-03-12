89d3c282e77d089a2fa250f60a14dbbb51288ae3
jade <jade.dasilva.lima.jds@gmail.com>
2023-03-12 07:20:27 -0400

    description part style

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -6,7 +6,7 @@
         innovative solutions to everyday problems.</p>
       <p>For whatever reason it is you are here, welcome.</p>
     </div>
-    <a href="/about">About me</a>
+    <a href="/about" class="link link__metis">About me</a>
   </div>
 </template>
 
@@ -15,9 +15,26 @@
 
 <style scoped>
 .home__description {
-  padding-top: 4rem;
-  font-size: 4rem;
-  font-weight: 400
+  padding-top: 5rem;
+  display: grid;
+  place-content: center;
+  height: 100vh;
+  max-height: 80rem;
+  width: min(100%,35rem);
+  margin: auto;
 }
 
+.home__description p {
+  font-size: 2.0rem;
+  line-height: 1.5;
+  padding-bottom: 1.5rem;
+  letter-spacing: 1px;
+}
+
+.home__description a {
+  font-size: 2.5rem;
+  width: fit-content;
+  margin-top: 1.5rem;
+  font-family: "ClashDisplayBold", sans-serif;
+}
 </style>
